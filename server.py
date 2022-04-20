56607764d15b2ba0582fef30fd5ae484575f15e9
Binwin Viju <binwinviju225096@egoogle.com>
2022-04-20 23:08:01 +0530

    updated code

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 
+import json
+from bson.objectid import ObjectId
+import pymongo
 from flask import Flask, Response, request
 app = Flask(__name__)
-import pymongo
-from bson.objectid import ObjectId
-import json
 
 
 ###########################################################################
@@ -13,10 +13,10 @@ try:
     mongo = pymongo.MongoClient(
         host="localhost",
         port=27017,
-        serverSelectionTimeoutMS = 1000
+        serverSelectionTimeoutMS=1000
     )
     db = mongo.company
-    mongo.server_info() # trigger exception if cannot connect to database
+    mongo.server_info()  # trigger exception if cannot connect to database
 except:
     print("ERROR - Cannot connect to db")
 
@@ -31,12 +31,89 @@ def get_some_users():
         for user in data:
             user["_id"] = str(user["_id"])
         return Response(response=json.dumps(data),
-        status=200,
-        mimetype="application/json")
+                        status=200,
+                        mimetype="application/json")
 
     except Exception as ex:
         print(ex)
-        Response(response=json.dumps({"message": "cannot read users"}),status=500,mimetype="application/json")
+        Response(response=json.dumps(
+            {"message": "cannot read users"}), status=500, mimetype="application/json")
+
+
+###########################################################################
+
+
+@app.route("/users/<id>", methods=["PATCH"])
+def update_users(id):
+    try:
+        dbResponse = db.users.update_one(
+            {"_id": ObjectId(id)},
+            {"$set": {
+                "name": request.form["name"],
+                "lastName": request.form["lastName"]
+            }}
+        )
+
+        if dbResponse.modified_count == 1:
+            return Response(
+                response=json.dumps(
+                    {"message": "User updated"}
+                ), status=200,
+                mimetype="application/json"
+            )
+
+        else:
+            return Response(
+                response=json.dumps(
+                    {"message": "Nothing to update"}
+                ), status=200,
+                mimetype="application/json"
+            )
+
+    except Exception as ex:
+        print("********************")
+        print(ex)
+        print("********************")
+        return Response(
+            response=json.dumps(
+                {"message": "sorry cannot update!!"}
+            ), status=500,
+            mimetype="application/json"
+        )
+
+
+###########################################################################
+
+
+@app.route("/users/<id>", methods=["DELETE"])
+def delete_user(id):
+
+    try:
+
+        dbResponse = db.users.delete_one(
+            {"_id": ObjectId(id)}
+        )
+
+        for attr in dir(dbResponse):
+            print("******{attr}*******")
+        return Response(
+            response=json.dumps(
+                {"message": "User Deleted", "id": f"{id}"}
+            ), status=200,
+            mimetype="application/json"
+        )
+
+    except Exception as ex:
+
+        print("********************")
+        print(ex)
+        print("********************")
+        return Response(
+            response=json.dumps(
+                {"message": "sorry cannot delete!!"}
+            ), status=500,
+            mimetype="application/json"
+        )
 
 
 ###########################################################################
@@ -51,12 +128,12 @@ def create_user():
         }
         dbResponse = db.users.insert_one(user)
         print(dbResponse.inserted_id)
-        #for attr in dir(dbResponse):
-            # print(attr)
+        # for attr in dir(dbResponse):
+        # print(attr)
         return Response(
             response=json.dumps(
-                {"message": "user created", 
-                "id":f"{dbResponse.inserted_id}"}),
+                {"message": "user created",
+                 "id": f"{dbResponse.inserted_id}"}),
             status=200,
             mimetype="application/json"
         )
@@ -68,4 +145,3 @@ def create_user():
 ###########################################################################
 if __name__ == "__main__":
     app.run(port=80, debug=True)
-
